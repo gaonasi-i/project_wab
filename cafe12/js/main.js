5ddad7f886a7d6c7454582188476bed58fca1725
gaonasi-i <dlrkdms73@naver.com>
2023-04-07 18:21:12 +0900

    submenu_teb

---
+++ cafe12/js/main.js
@@ -0,0 +1,28 @@
+//시계
+window.onload = function(){
+setInterval(setWatch, 1000);
+function setWatch(){
+    const date = new Date();
+    let now = date.toLocaleTimeString(); //시간
+    document.getElementById("demo").innerHTML = now;
+}
+
+//배경 이미지 Change
+let picture = ["./imges/header1.jpg", "./imges/header2.jpg",
+"./imges/header3.jpg"];
+
+let imgIdx = 0;
+
+showPicture();
+
+function showPicture(){
+    let img = document.getElementById("pic");
+    img.src = picture[imgIdx];
+    imgIdx++;
+
+    if(imgIdx == picture.length){
+        imgIdx = 0;
+    }
+    setTimeout(showPicture, 2000); //콜백 함수
+}
+}
